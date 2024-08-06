12b30be6842a4dcabdff21a2ecca21ce48caaf95
taichomeiso <otaichididit@gmail.com>
2024-08-06 19:12:57 +0900

    googleアカウントで登録、ログインができる
    新規登録画面の見た目を少し整えている

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   get 'threads/index'
   root to: "threads#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_062101) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_034926) do
+  create_table "sns_credentials", charset: "utf8mb3", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_062101) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sns_credentials", "users"
 end
